44e6ebf28f416289180c0f42f32760eda1eda923
vipul-zambare006 <vipulzambare1993@gmail.com>
2019-11-10 00:34:14 +0800

    Fixes in layout

--- index.js
+++ index.js
@@ -7,67 +7,67 @@ jQuery(document).ready(function($) {
   var width = (images / 4) * 102;
 
   imageNumberURLMap = {
-    "1": "https://www.facebook.com/",
-    "2": "https://www.quora.com/",
-    "3": "https://www.twitter.com/",
-    "4": "https://www.facebook.com/",
-    "5": "https://www.quora.com/",
-    "6": "https://www.twitter.com/",
-    "7": "https://www.facebook.com/",
-    "8": "https://www.quora.com/",
-    "9": "https://www.twitter.com/",
-    "10": "https://www.facebook.com/",
-    "11": "https://www.quora.com/",
-    "12": "https://www.twitter.com/",
-    "13": "https://www.facebook.com/",
-    "14": "https://www.quora.com/",
-    "15": "https://www.twitter.com/",
-    "16": "https://www.facebook.com/",
-    "17": "https://www.quora.com/",
-    "18": "https://www.twitter.com/",
-    "19": "https://www.facebook.com/",
-    "20": "https://www.quora.com/",
-    "21": "https://www.quora.com/",
-    "22": "https://www.twitter.com/",
-    "23": "https://www.facebook.com/",
-    "24": "https://www.quora.com/",
-    "25": "https://www.twitter.com/",
-    "26": "https://www.facebook.com/",
-    "27": "https://www.quora.com/",
-    "28": "https://www.twitter.com/",
-    "29": "https://www.facebook.com/",
-    "30": "https://www.quora.com/",
-    "31": "https://www.facebook.com/",
-    "32": "https://www.quora.com/"
+    "1": "http://entice.bbem.in/about-us/",
+    "2": "http://entice.bbem.in/about-us/",
+    "3": "http://entice.bbem.in/about-us/",
+    "4": "http://entice.bbem.in/about-us/",
+    "5": "http://entice.bbem.in/about-us/",
+    "6": "http://entice.bbem.in/about-us/",
+    "7": "http://entice.bbem.in/about-us/",
+    "8": "http://entice.bbem.in/contact-us/",
+    "9": "http://entice.bbem.in/contact-us/",
+    "10": "http://entice.bbem.in/contact-us/",
+    "11": "http://entice.bbem.in/contact-us/",
+    "12": "http://entice.bbem.in/contact-us/",
+    "13": "http://entice.bbem.in/contact-us/",
+    "14": "http://entice.bbem.in/contact-us/",
+    "15": "http://entice.bbem.in/contact-us/",
+    "16": "http://entice.bbem.in/contact-us/",
+    "17": "http://entice.bbem.in/contact-us/",
+    "18": "http://entice.bbem.in/contact-us/",
+    "19": "http://entice.bbem.in/contact-us/",
+    "20": "http://entice.bbem.in/contact-us/",
+    "21": "http://entice.bbem.in/contact-us/",
+    "22": "http://entice.bbem.in/contact-us/",
+    "23": "http://entice.bbem.in/contact-us/",
+    "24": "http://entice.bbem.in/contact-us/",
+    "25": "http://entice.bbem.in/contact-us/",
+    "26": "http://entice.bbem.in/contact-us/",
+    "27": "http://entice.bbem.in/contact-us/",
+    "28": "http://entice.bbem.in/contact-us/",
+    "29": "http://entice.bbem.in/contact-us/",
+    "30": "http://entice.bbem.in/contact-us/",
+    "31": "http://entice.bbem.in/contact-us/",
+    "32": "http://entice.bbem.in/contact-us/"
   };
 
-  $content.width(width);
+  $content.width(width - 60);
 
-  console.log($content);
+  // console.log($content);
 
   $view.css({
     height: $win.height(),
-    width: "60%"
+    width: "60%" //MAKE IT 60% to before deploy
   });
 
   for (var i = 1; i < 33; i += 1) {
-    // let imgNumber = Math.floor(Math.random() * 4) + 1;
     let imagePath = `http://entice.bbem.in/wp-content/themes/Divi/images/${i}.jpg`;
     let imgClass = "";
-    // if (i === 6) {
-    //   imagePath = `http://entice.bbem.in/wp-content/themes/Divi/images/logo.jpg`;
-    //   imgClass = "logo";
-    // }
 
     var $image = $("<img>", {
       src: imagePath,
-      click: function() {
-        redirectImgURL(i);
-      },
       class: imgClass
     });
 
-    $image.appendTo($content);
+    var productLink = imageNumberURLMap[i.toString()];
+
+    var anchorTag = $("<a></a>")
+      .attr("id", i)
+      .attr("href", productLink)
+      .attr("target", "_black");
+
+    anchorTag.appendTo($content);
+    $image.appendTo(anchorTag);
   }
 
   $view.on("mousemove", function(event) {
@@ -76,45 +76,40 @@ jQuery(document).ready(function($) {
     var mouseX = event.pageX - parentOffset.left;
     var mouseY = event.pageY - parentOffset.top;
 
-    console.log("mouse x", mouseX);
-    console.log("mouseY", mouseY);
+    // console.log("mouse x", mouseX);
+    // console.log("mouseY", mouseY);
 
     $log.text(mouseX + ", " + mouseY);
 
     var viewRangeX = $this.width();
     var viewRangeY = $this.height();
 
-    console.log("viewRange X:", viewRangeX);
-    console.log("viewRange Y:", viewRangeY);
+    // console.log("viewRange X:", viewRangeX);
+    // console.log("viewRange Y:", viewRangeY);
 
     var contentRangeX = $content.outerWidth(true);
     var contentRangeY = $content.outerHeight(true);
 
-    console.log("contentRange X:", contentRangeX);
-    console.log("contentRange Y:", contentRangeY);
+    // console.log("contentRange X:", contentRangeX);
+    // console.log("contentRange Y:", contentRangeY);
 
     var ratioX = contentRangeX / viewRangeX;
     var ratioY = contentRangeY / viewRangeY;
-
-    // var differenceX = contentRangeX - viewRangeX;
-    // var differenceY = contentRangeY - viewRangeY;
-
     var offsetX = mouseX * ratioX - mouseX;
     var offsetY = mouseY * ratioY - mouseY;
 
-    console.log("offsetX", offsetX);
-    console.log("offsetY", offsetY);
+    // console.log("offsetX", offsetX);
+    // console.log("offsetY", offsetY);
 
     $content.css({
       transform:
-        "translate(" + offsetX * -0.62 + "px" + ", " + offsetY * -1 + "px" + ")"
-      // top: (offsetY * -1) + 'px',
-      // left: ((offsetX * -1) + 200) + 'px'
+        "translate(" +
+        offsetX * -0.56 +
+        "px" +
+        ", " +
+        offsetY * -1.04 +
+        "px" +
+        ")"
     });
   });
-
-  function redirectImgURL(imageNumber) {
-    var imageNumberStr = imageNumber.toString();
-    window.location.href = imageNumberURLMap[imageNumberStr];
-  }
 });
